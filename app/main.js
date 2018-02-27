fd40c8bd767e7fb6443f0be62deea73014f9920b
luanhualiang <luanhualiang@dianmi365.com>
2018-02-27 15:06:28 +0800

    文件下载调整，版本号更新为1.3.2

--- app/main.js
+++ app/main.js
@@ -1,7 +1,9 @@
 import "babel-polyfill"
-import {app, BrowserWindow, BrowserView, autoUpdater} from 'electron'
+import {app, dialog, BrowserWindow} from 'electron'
 import path from 'path'
 import url from 'url'
+import request from 'request'
+import fs from 'fs' 
 import mTray from './tray'
 import menu from './menu'
 import checkUpdate from './update'
@@ -34,7 +36,7 @@ function createWindow() {
             defaultFontSize: 14
         }
     })
-    mainWindow.loadURL('https://2haohr.com/login')
+    mainWindow.loadURL('http://localhost:16001/login')
     // mainWindow.loadURL(url.format({
     //     pathname: resolve('../app/index.html'),
     //     protocol: 'file:',
@@ -69,6 +71,35 @@ function createWindow() {
     //         });
     //     }
     // });
+    //客户端对于下载文件重命名时，对于文件扩展名的处理会不那么友好，做如下处理
+    mainWindow.webContents.session.on('will-download', (e, item) => {
+        e.preventDefault()
+        //获取文件的总大小
+        const totalBytes = item.getTotalBytes();
+        
+        //设置文件的保存路径，此时默认弹出的 save dialog 将被覆盖
+        const fileName = item.getFilename()
+        const filePath = path.resolve(app.getPath('downloads'), item.getFilename())
+        const fileUrl = item.getURL()
+        const ext = path.parse(filePath).ext
+        dialog.showSaveDialog({
+            defaultPath: filePath,
+            nameFieldLabel: fileName,
+            filters: [
+                { name: fileName, extensions: ext }
+            ]
+        }, fname => {
+            if(fname){
+                request.get({
+                    uri: fileUrl,
+                    encoding: 'binary'
+                }, (error,reponse,data)=> {
+                    let fp = path.resolve(app.getPath('downloads'), fname)
+                    fs.writeFileSync(fp,data,'binary')
+                })
+            }
+        });
+    });
     tray = new mTray(app,mainWindow)
     menu(app,mainWindow)
     app.setLoginItemSettings({ //设置开启启动
